ac85ddf9f13b4663792c0997f6e2001bdee390ea
Michał Chawiński <m.chawinski@gmail.com>
2024-12-18 12:41:53 +0100

    Adding issues.py

--- testingOperationsOnPresence.py
+++ testingOperationsOnPresence.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import csv
+# TODO: F401 [*] `unittest.mock.patch` imported but unused, help: Remove unused import: `unittest.mock.patch`
 from unittest.mock import patch
 from check_attendance import CheckAttendanceClass
 from managingList import managingListClass
